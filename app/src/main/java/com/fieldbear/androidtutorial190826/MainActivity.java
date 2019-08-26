035e7a2f2fb16151b40b24d7a776d4a43a99971c
jh <jhjh550@gmail.com>
2019-08-26 10:52:01 +0900

    button

--- app/src/main/java/com/fieldbear/androidtutorial190826/MainActivity.java
+++ app/src/main/java/com/fieldbear/androidtutorial190826/MainActivity.java
@@ -3,12 +3,24 @@ package com.fieldbear.androidtutorial190826;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_widget);
+
+        Button btnButton = findViewById(R.id.btnButton);
+        btnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "button clicked",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
