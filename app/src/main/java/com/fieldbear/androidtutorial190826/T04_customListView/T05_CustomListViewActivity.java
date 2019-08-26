5125d072ba8e2af883a390156eac44cfda4d9b6f
jh <jhjh550@gmail.com>
2019-08-26 16:11:01 +0900

    custom list view - data

--- app/src/main/java/com/fieldbear/androidtutorial190826/T04_customListView/T05_CustomListViewActivity.java
+++ app/src/main/java/com/fieldbear/androidtutorial190826/T04_customListView/T05_CustomListViewActivity.java
@@ -15,14 +15,30 @@ public class T05_CustomListViewActivity extends AppCompatActivity {
         String weatherText;
         int temperature;
         int imgRes;
+
+        public Weather(String cityName, String weatherText, int temperature, int imgRes) {
+            this.cityName = cityName;
+            this.weatherText = weatherText;
+            this.temperature = temperature;
+            this.imgRes = imgRes;
+        }
     }
 
     ArrayList<Weather> list = new ArrayList();
 
+    private void initData(){
+        list.add(new Weather("Berlin", "snowing",0, R.drawable.ic_snow));
+        list.add(new Weather("Bangalore", "Thunderstorms",23, R.drawable.ic_thunder));
+        list.add(new Weather("London", "rainy",5, R.drawable.ic_rainy));
+        list.add(new Weather("New York", "cloudy",18, R.drawable.ic_cloud_black_24dp));
+        list.add(new Weather("Sydney", "sunny",32, R.drawable.ic_wb_sunny_black_24dp));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_t05__custom_list_view);
+
+        initData();
     }
 }
