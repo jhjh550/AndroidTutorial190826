1dfaae4d74727d2cf24178094f692aa78ead41b8
jh <jhjh550@gmail.com>
2019-08-26 15:18:15 +0900

    listview

---
+++ app/src/main/java/com/fieldbear/androidtutorial190826/T03_listview/T03_ListActivity.java
@@ -0,0 +1,16 @@
+package com.fieldbear.androidtutorial190826.T03_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fieldbear.androidtutorial190826.R;
+
+public class T03_ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_t03__list);
+    }
+}
