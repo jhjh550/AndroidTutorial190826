121473d9877873d439efa2c02dbfeb4c60bad175
jh <jhjh550@gmail.com>
2019-08-26 17:21:48 +0900

    shared preferences - custom list view

---
+++ app/src/main/java/com/fieldbear/androidtutorial190826/T05_SharedPreferences/T05_SharedPrefenencesActivity.java
@@ -0,0 +1,16 @@
+package com.fieldbear.androidtutorial190826.T05_SharedPreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fieldbear.androidtutorial190826.R;
+
+public class T05_SharedPrefenencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_t05__shared_prefenences);
+    }
+}
