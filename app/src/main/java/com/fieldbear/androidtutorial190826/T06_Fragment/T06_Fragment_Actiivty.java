5da2f3ded0b64938f7809e235632e0a4a3140e79
jh <jhjh550@gmail.com>
2019-08-27 09:52:37 +0900

    fragment

---
+++ app/src/main/java/com/fieldbear/androidtutorial190826/T06_Fragment/T06_Fragment_Actiivty.java
@@ -0,0 +1,16 @@
+package com.fieldbear.androidtutorial190826.T06_Fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fieldbear.androidtutorial190826.R;
+
+public class T06_Fragment_Actiivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_t06__fragment__actiivty);
+    }
+}
