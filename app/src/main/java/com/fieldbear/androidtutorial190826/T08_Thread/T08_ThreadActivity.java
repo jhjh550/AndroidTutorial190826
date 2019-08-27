4454ae5997646678badba1678fceda09314893cd
jh <jhjh550@gmail.com>
2019-08-27 14:28:57 +0900

    thread/handler

--- app/src/main/java/com/fieldbear/androidtutorial190826/T08_Thread/T08_ThreadActivity.java
+++ app/src/main/java/com/fieldbear/androidtutorial190826/T08_Thread/T08_ThreadActivity.java
@@ -3,6 +3,8 @@ package com.fieldbear.androidtutorial190826.T08_Thread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,20 +14,43 @@ import com.fieldbear.androidtutorial190826.R;
 
 public class T08_ThreadActivity extends AppCompatActivity {
 
+    private static final int MY_COUNT = 100;
+    Button btnStart;
+    Handler myHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            if(msg.what == MY_COUNT) {
+                btnStart.setText(msg.arg1+"");
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_t08__thread);
 
-        Button btnStart = findViewById(R.id.btnStart);
+        btnStart = findViewById(R.id.btnStart);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        for(int i=0; i<100000; i++){
+                        for(int i=0; i<100; i++){
+                            try {
+                                Thread.sleep(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
                             Log.d("MyThread", "count : "+i);
+                            Message msg = myHandler.obtainMessage();
+                            msg.what = MY_COUNT;
+                            msg.arg1 = i;
+
+                            myHandler.sendMessage(msg);
                         }
                     }
                 }).start();
