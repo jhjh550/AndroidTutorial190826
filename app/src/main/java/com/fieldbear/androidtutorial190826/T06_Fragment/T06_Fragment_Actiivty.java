d17a514295fe1d0b82d41a2904bb849700056ef7
jh <jhjh550@gmail.com>
2019-08-27 10:42:02 +0900

    new fragment add

--- app/src/main/java/com/fieldbear/androidtutorial190826/T06_Fragment/T06_Fragment_Actiivty.java
+++ app/src/main/java/com/fieldbear/androidtutorial190826/T06_Fragment/T06_Fragment_Actiivty.java
@@ -1,16 +1,44 @@
 package com.fieldbear.androidtutorial190826.T06_Fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.fieldbear.androidtutorial190826.R;
 
-public class T06_Fragment_Actiivty extends AppCompatActivity {
+public class T06_Fragment_Actiivty extends AppCompatActivity
+                                implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_t06__fragment__actiivty);
+
+        findViewById(R.id.btnAdd).setOnClickListener(this);
+        findViewById(R.id.btnRemove).setOnClickListener(this);
+        findViewById(R.id.btnReplace).setOnClickListener(this);
+        findViewById(R.id.btnHide).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.frame);
+
+        switch (v.getId()){
+            case R.id.btnAdd:
+                if(fragment == null){
+                   fragment = new CounterFragment();
+                   FragmentTransaction transaction = fm.beginTransaction();
+                   transaction.add(R.id.frame, fragment,"counter");
+                   transaction.commit();
+                }
+                break;
+
+        }
     }
 }
