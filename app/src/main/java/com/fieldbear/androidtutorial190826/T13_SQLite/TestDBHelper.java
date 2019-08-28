8dfa4fd70785a2ec7642a260c8bdae61f8765cd6
jh <jhjh550@gmail.com>
2019-08-28 09:57:14 +0900

    sqlite

--- app/src/main/java/com/fieldbear/androidtutorial190826/T13_SQLite/TestDBHelper.java
+++ app/src/main/java/com/fieldbear/androidtutorial190826/T13_SQLite/TestDBHelper.java
@@ -1,6 +1,9 @@
 package com.fieldbear.androidtutorial190826.T13_SQLite;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 public class TestDBHelper {
     TestSQLiteOpenHelper mOpenHelper;
@@ -9,4 +12,48 @@ public class TestDBHelper {
         mOpenHelper = new TestSQLiteOpenHelper(context, "people.db",
                 null, 1);
     }
+
+    public void insert(String name, int age, String address){
+        SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("name", name);
+        values.put("age", age);
+        values.put("address", address);
+        db.insert("student", null, values);
+    }
+
+    public void delete(String name){
+        SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        db.delete("student", "name = ?",
+                new String[]{name});
+    }
+
+    public void update(String name, int newAge){
+        SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("age", newAge);
+        db.update("student", values, "name = ?",
+                new String[]{name});
+    }
+
+    public String selectAll(){
+        String res = "";
+        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        Cursor c = db.query("student", null, null, null,
+                null, null, null);
+
+        while(c.moveToNext()){
+            String name = c.getString( c.getColumnIndex("name") );
+            int age = c.getInt( c.getColumnIndex("age") );
+            String address = c.getString( c.getColumnIndex("address") );
+
+            res += "name : "+name+" age: "+age+" address: "+address+"\n";
+        }
+
+        return res;
+    }
+
+
+
+
 }
