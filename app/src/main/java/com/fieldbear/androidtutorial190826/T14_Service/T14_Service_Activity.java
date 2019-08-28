f1f054a97f42119747d525b778dad00149039d0e
jh <jhjh550@gmail.com>
2019-08-28 10:43:34 +0900

    started type service

---
+++ app/src/main/java/com/fieldbear/androidtutorial190826/T14_Service/T14_Service_Activity.java
@@ -0,0 +1,41 @@
+package com.fieldbear.androidtutorial190826.T14_Service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.fieldbear.androidtutorial190826.R;
+
+public class T14_Service_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_t14__service_);
+
+        Button btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        T14_Service_Activity.this,
+                        MyService.class);
+                startService(intent);
+            }
+        });
+
+        Button btnStop = findViewById(R.id.btnStop);
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        T14_Service_Activity.this,
+                        MyService.class);
+                stopService(intent);
+            }
+        });
+    }
+}
