a6bc57a4b164dada3b0eabf7a275c2186a09f853
jh <jhjh550@gmail.com>
2019-08-28 10:01:04 +0900

    sqlite

--- app/src/main/java/com/fieldbear/androidtutorial190826/T13_SQLite/T13_SQLiteActivity.java
+++ app/src/main/java/com/fieldbear/androidtutorial190826/T13_SQLite/T13_SQLiteActivity.java
@@ -3,6 +3,7 @@ package com.fieldbear.androidtutorial190826.T13_SQLite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.fieldbear.androidtutorial190826.R;
 
@@ -12,5 +13,17 @@ public class T13_SQLiteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_t13__sqlite);
+
+        TestDBHelper helper = new TestDBHelper(this);
+        helper.insert("kim", 11, "서울");
+        helper.insert("lee", 22, "busan");
+        helper.insert("park", 33, "인천");
+
+        helper.update("kim", 13);
+        String res = helper.selectAll();
+
+        TextView textViewResult = findViewById(R.id.textViewResult);
+        textViewResult.setText(res);
+
     }
 }
